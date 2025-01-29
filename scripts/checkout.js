d627948e03956d3d3754f793d47ee699a2d0790f
pvntheraxxx <pvntheraxxx@gmail.com>
2025-01-29 19:05:20 +0300

    Delete products from cart when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart, removeFromCart} from '../data/cart.js';
+import {cart, removeFromCart} from '.cart.js';
 import {products} from '../data/products.js';
 import {formatCurrency} from './utils/money.js';
 
