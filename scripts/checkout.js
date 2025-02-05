a5211e385a254f7be07272753a2496ac986cfaca
pvntheraxxx <pvntheraxxx@gmail.com>
2025-02-06 00:35:50 +0300

    Convert from promises into async

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,28 +1,50 @@
 import {renderOrderSummary} from './checkout/orderSummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
-import { loadProducts, loadProductsFetch } from '../data/products.js';
-import { loadCart } from '../data/cart.js';
-// import '../data/cart-oop.js';
+import {loadProducts, loadProductsFetch} from '../data/products.js';
+import {loadCart} from '../data/cart.js';
+// import '../data/cart-class.js';
 // import '../data/backend-practice.js';
 
+async function loadPage() {
+  try {
+    // throw 'error1';
+
+    await loadProductsFetch();
+
+    const value = await new Promise((resolve, reject) => {
+      // throw 'error2';
+      loadCart(() => {
+        // reject('error3');
+        resolve('value3');
+      });
+    });
+
+  } catch (error) {
+    console.log('Unexpected error. Please try again later.');
+  }
+
+  renderOrderSummary();
+  renderPaymentSummary();
+}
+loadPage();
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve) => {
-    loadProducts(() => {
-      resolve('value1');
-    });
-  }),
-  new Promise((resolve) => {
     loadCart(() => {
       resolve();
-    }); 
+    });
   })
+
 ]).then((values) => {
   console.log(values);
   renderOrderSummary();
   renderPaymentSummary();
-})
+});
+*/
 
+/*
 new Promise((resolve) => {
   loadProducts(() => {
     resolve('value1');
@@ -30,20 +52,24 @@ new Promise((resolve) => {
 
 }).then((value) => {
   console.log(value);
-    return new Promise((resolve) => {
-      loadCart(() => {
-        resolve();
-      }); 
+
+  return new Promise((resolve) => {
+    loadCart(() => {
+      resolve();
     });
+  });
 
 }).then(() => {
   renderOrderSummary();
   renderPaymentSummary();
 });
+*/
 
-// loadProducts(() => {
-//   loadCart(() => {
-//     renderOrderSummary();
-//     renderPaymentSummary();
-//   })  
-// })
+/*
+loadProducts(() => {
+  loadCart(() => {
+    renderOrderSummary();
+    renderPaymentSummary();
+  });
+});
+*/
